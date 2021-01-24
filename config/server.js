d415b3f777fadb8ea43f396bcd82b22677aa88f7
wachira_Dev <ewachira254@gmail.com>
2021-01-24 23:49:43 +0300

    🐞 @roko/kafri: fix url bug on cms

--- config/server.js
+++ config/server.js
@@ -5,5 +5,9 @@ module.exports = ({ env }) => ({
     auth: {
       secret: env("ADMIN_JWT_SECRET", "c49d34db847306593d94a937eb375a4d"),
     },
-  }
+  },
+  url:
+    env("NODE_ENV") === "production"
+      ? "https://kafri-cms.roko.store"
+      : "http://localhost:1337",
 });
